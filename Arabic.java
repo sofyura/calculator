dd0bc64f1d1cba5d673ce1595394c27df4cd4257
sofyura <95036984+sofyura@users.noreply.github.com>
2022-10-24 23:34:19 +0300

    Add files via upload

--- Arabic.java
+++ Arabic.java
@@ -1,47 +1,78 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arabic {
-        public void run(){
-            Scanner console = new Scanner(System.in);
-            String str = console.nextLine();
+    String str;
 
-            int len = str.length();
-            int pluse = str.indexOf("+");
-            int minus = str.indexOf("-");
-            int multiplication = str.indexOf("*");
-            int division = str.indexOf("/");
+    Byte x1;
+    Byte x2;
 
-            if (len < 3 || len > 5) {
-                System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-            }
-            if (len >= 3 && len < 5) {
-                for (int c = 1; c < 3; c++) {
-                    if (pluse == c) {
-                        String[] s = str.split("\\+");
-                        Byte x1 = Byte.valueOf(s[0]);
-                        Byte x2 = Byte.valueOf(s[1]);
+    Arabic(String str) {
+        this.str = str;
+    }
+
+    public void run() {
+
+        int len = str.length();
+        int pluse = str.indexOf("+");
+        int minus = str.indexOf("-");
+        int multiplication = str.indexOf("*");
+        int division = str.indexOf("/");
+
+
+        if (len >= 3 && len < 5) {
+            for (int c = 1; c < 3; c++) {
+                if (pluse == c) {
+                    String[] s = str.split("\\+");
+
+                    x1 = Byte.valueOf(s[0]);
+                    x2 = Byte.valueOf(s[1]);
+                    if((x1<0 || x1>10)||(x2<0||x2>10)){
+                        throw new Error("throws Exception");
+                    }
+                    else {
                         System.out.print(x1 + x2);
                     }
-                    if (minus == c) {
-                        String[] s = str.split("-");
-                        Byte x1 = Byte.valueOf(s[0]);
-                        Byte x2 = Byte.valueOf(s[1]);
+                }
+                if (minus == c) {
+                    String[] s = str.split("-");
+                    x1 = Byte.valueOf(s[0]);
+                    x2 = Byte.valueOf(s[1]);
+                    if((x1<0 || x1>10)||(x2<0||x2>10)){
+                        throw new Error("throws Exception");
+                    }
+                    else {
                         System.out.print(x1 - x2);
                     }
-                    if (multiplication == c) {
-                        String[] s = str.split("\\*");
-                        Byte x1 = Byte.valueOf(s[0]);
-                        Byte x2 = Byte.valueOf(s[1]);
+                }
+                if (multiplication == c) {
+                    String[] s = str.split("\\*");
+                    x1 = Byte.valueOf(s[0]);
+                    x2 = Byte.valueOf(s[1]);
+                    if((x1<0 || x1>10)||(x2<0||x2>10)){
+                        throw new Error("throws Exception");
+                    }
+                    else {
                         System.out.print(x1 * x2);
                     }
-                    if (division == c) {
-                        String[] s = str.split("/");
-                        Byte x1 = Byte.valueOf(s[0]);
-                        Byte x2 = Byte.valueOf(s[1]);
+                }
+                if (division == c) {
+                    String[] s = str.split("/");
+                    x1 = Byte.valueOf(s[0]);
+                    x2 = Byte.valueOf(s[1]);
+                    if((x1<0 || x1>10)||(x2<0||x2>10)){
+                        throw new Error("throws Exception");
+                    }
+                    else {
                         System.out.print(x1 / x2);
                     }
                 }
             }
+        } else {
+            throw new Error("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
+
     }
+}
+
 
--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Arabic arabic = new Arabic();
-        arabic.run();
-        Roman roman = new Roman();
-        roman.run();
+        new Scan();
     }
 }
